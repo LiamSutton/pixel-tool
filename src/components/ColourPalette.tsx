1761689268c30c884935a54dd2cfa29d264a7c70
Liam Sutton <liamsutton1997@outlook.com>
2022-10-23 18:08:09 +0100

    update editor colour using colour palette

--- src/components/ColourPalette.tsx
+++ src/components/ColourPalette.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useState } from 'react';
 import '../css/ColourPalette.css';
+import { IColourPalette } from '../interfaces/IColourPalette';
 
-const ColourPalette = () => {
+const ColourPalette: FC<IColourPalette> = (props) => {
     return(
-        <div>
-            colour palette component
+        <div className='ColourPalette'>
+            <button onClick={() => props.updateColour('#000000')}>Colour = Black</button>
+            <button onClick={() => props.updateColour('#FF0000')}>Colour = Red </button>
+            <button onClick={() => props.updateColour('#00FF00')}>Colour = Green</button>
         </div>
     )
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,16 @@
 import React, { FC, useState } from 'react';
 import '../css/Editor.css'
 import { IEditor } from '../interfaces/IEditor';
+import ColourPalette from './ColourPalette';
 
 const Editor: FC<IEditor> = (props) => {
     const [colour, setColour] = useState('#FFFFFF')
     return(
         <div>
+            <h1>Color {colour}</h1>
+            <div style={{height: 50, width: 50, backgroundColor: colour}}>
+            </div>
+            <ColourPalette updateColour={setColour}/>
         </div>
     )
 }
