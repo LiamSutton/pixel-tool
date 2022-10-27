8dc3e65025691fc95e5e3ed09bfd4614134846d0
Liam Sutton <liamsutton1997@outlook.com>
2022-10-27 13:39:00 +0100

    colour palette items now appear as coloured squares

--- src/components/ColourPalette.tsx
+++ src/components/ColourPalette.tsx
@@ -5,9 +5,9 @@ import { IColourPalette } from '../interfaces/IColourPalette';
 const ColourPalette: FC<IColourPalette> = (props) => {
     return(
         <div className='ColourPalette'>
-            <button onClick={() => props.updateColour('#000000')}>Colour = Black</button>
-            <button onClick={() => props.updateColour('#FF0000')}>Colour = Red </button>
-            <button onClick={() => props.updateColour('#00FF00')}>Colour = Green</button>
+            <div className='ColourPaletteItem' style={{backgroundColor: '#000000'}} onClick={() => props.updateColour('#000000')}></div>
+            <div className='ColourPaletteItem' style={{backgroundColor: '#FF0000'}} onClick={() => props.updateColour('#FF0000')}></div>
+            <div className='ColourPaletteItem' style={{backgroundColor: '#00FF00'}} onClick={() => props.updateColour('#00FF00')}></div>
         </div>
     )
 }
